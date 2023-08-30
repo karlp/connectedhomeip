karlp/connectedhomeip

commit a0cb2705ece755f1f0b7f20081dc3d46fd361b82

examples/lock-app/esp32: synchronize buttons to matter (#27270)
Appears to have been badly copy/pasted from other examples perhaps.
Without this update call, pushing the button locally will update the
LEDs and print the messages, but will not update the matter cluster
state.

Signed-off-by: Karl Palsson <karlp@tweak.au>

examples/lock-app/esp32/main/AppTask.cpp
@@ -422,6 +422,11 @@ void AppTask::ActionCompleted(BoltLockManager::Action_t aAction)
 
         sLockLED.Set(false);
     }
+    if (sAppTask.mSyncClusterToButtonAction)
+    {
+        chip::DeviceLayer::SystemLayer().ScheduleWork(UpdateClusterState, nullptr);
+        sAppTask.mSyncClusterToButtonAction = false;
+    }
 }
 
 void AppTask::PostLockActionRequest(int32_t aActor, BoltLockManager::Action_t aAction)

commit 3f162b3a2bca9e0e9ef7887708660bfc62fd5f94

FIXME: examples/lock-app/esp32: Fix synchronisation crash
Fixes: a0cb2705 #27270
The earlier fix had been developed against matter 1.0 branch, so the
incorrect style of scheduling to avoid lock checking was used.

Use the same style (via PlatformMgr, not SystemLayer) as all other
examples.

Signed-off-by: Karl Palsson <karlp@tweak.au>

FIXME: this fixes the crash, but we're still not synchronizing to HASS
anymore!

examples/lock-app/esp32/main/AppTask.cpp
@@ -99,7 +99,7 @@ CHIP_ERROR AppTask::Init()
 
     sLockLED.Set(!BoltLockMgr().IsUnlocked());
 
-    chip::DeviceLayer::SystemLayer().ScheduleWork(UpdateClusterState, nullptr);
+    chip::DeviceLayer::PlatformMgr().ScheduleWork(UpdateClusterState, 0);
 
     ConfigurationMgr().LogDeviceConfig();
 
@@ -424,7 +424,7 @@ void AppTask::ActionCompleted(BoltLockManager::Action_t aAction)
     }
     if (sAppTask.mSyncClusterToButtonAction)
     {
-        chip::DeviceLayer::SystemLayer().ScheduleWork(UpdateClusterState, nullptr);
+        chip::DeviceLayer::PlatformMgr().ScheduleWork(UpdateClusterState, 0);
         sAppTask.mSyncClusterToButtonAction = false;
     }
 }
@@ -463,7 +463,7 @@ void AppTask::DispatchEvent(AppEvent * aEvent)
 }
 
 /* if unlocked then it locked it first*/
-void AppTask::UpdateClusterState(chip::System::Layer *, void * context)
+void AppTask::UpdateClusterState(intptr_t context)
 {
     uint8_t newValue = !BoltLockMgr().IsUnlocked();
 

examples/lock-app/esp32/main/include/AppTask.h
@@ -66,7 +66,7 @@ private:
     static void LockActionEventHandler(AppEvent * aEvent);
     static void TimerEventHandler(TimerHandle_t xTimer);
 
-    static void UpdateClusterState(chip::System::Layer *, void * context);
+    static void UpdateClusterState(intptr_t context);
 
     void StartTimer(uint32_t aTimeoutMs);
 